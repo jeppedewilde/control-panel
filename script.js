e266da480cadd926c59408b5f2be65c1915171d2
jeppedewilde <jeppedewilde@gmail.com>
2026-03-12 15:59:58 +0100

    donderdag

---
+++ script.js
@@ -0,0 +1,18 @@
+// slider (bron: Sanne)
+console.log('hallo');
+
+const ranges = document.querySelectorAll('[type="range"]');
+
+const updateRange = range => {
+	const rangeName = range.name;
+	const rangeValue = range.value;
+	document.documentElement.style.setProperty("--"+rangeName, rangeValue);
+}
+
+ranges.forEach(range => {
+	updateRange(range);
+	
+	range.oninput = () => {
+		updateRange(range)
+	};
+});
